0bf6396ed9171db75d58ac1ce6558c4a5487cf43
Leandro Moren <leandro.moren@outlook.com>
2024-07-19 17:44:28 -0300

    Se agrego title dinamico para todas las pestanias

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,7 +2,7 @@
 export default function Head() {
   return (
     <header>
-      <title>{"Qavala - " + obtenerTituloEnProduccion()}</title>
+      <title>{"Qavala - " + obtenerTituloEnProduccion()?.toString()}</title>
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1.0"
