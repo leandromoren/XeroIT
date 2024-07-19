e01bc2c0007c9f188def7c44d2464af0ae53cf62
Leandro Moren <leandro.moren@outlook.com>
2024-07-19 18:11:04 -0300

    Se agrego title dinamico para todas las pestanias 6

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -12,6 +12,50 @@ export default function Head() {
       setCurrentUrl(window.location.href);
     }
   }, []);
+
+  function obtenerTituloEnProduccion() {
+    const fullUrl = currentUrl;
+    const BASE_PATH = "https://qavala.vercel.app/";
+    switch (fullUrl) {
+      case BASE_PATH:
+        return "Inicio";
+      case BASE_PATH + "nosotros/nuestro-equipo":
+        return "Nuestro equipo";
+      case BASE_PATH + "nosotros/nuestra-historia":
+        return "Nuestra historia";
+      case BASE_PATH + "nosotros/nuestros-valores":
+        return "Nuestros valores";
+      case BASE_PATH + "nosotros/nuestras-oficinas":
+        return "Nuestras oficinas";
+      case BASE_PATH + "nosotros/partners":
+        return "Partners";
+      case BASE_PATH +"publicaciones/historia-de-clientes":
+        return "Historia de clientes";
+      case BASE_PATH + "publicaciones/eventos":
+        return "Eventos";
+      case BASE_PATH + "empleo/programa-de-practicas":
+        return "Programa de practicas";
+      case BASE_PATH + "empleo/vacantes":
+        return "Vacantes";
+      case BASE_PATH + "empleo/trabaja-con-nosotros":
+        return "Trabaja con nosotros";
+      case BASE_PATH + "empleo/subir-curriculum":
+        return "Subir curriculum";
+      case BASE_PATH + "servicios/software-testing":
+        return "Software testing";
+      case BASE_PATH + "servicios/reclutamiento-it":
+        return "Reclutamiento IT";
+      case BASE_PATH + "servicios/gestion-de-proyectos":
+        return "Gestion de proyectos";
+      case BASE_PATH + "servicios/ux-ui":
+        return "UX / UI";
+      case BASE_PATH + "servicios/desarrollo-software":
+        return "Desarrollo de software";
+      default:
+        return "Qavala";
+    }
+  }
+
   return (
     <header>
       <title>{obtenerTituloEnProduccion()}</title>
@@ -31,45 +75,4 @@ export default function Head() {
       />
     </header>
   );
-}
-
-function obtenerTituloEnProduccion() {
-  const fullUrl = window.location.href;
-  const BASE_PATH = "http://qavala.vercel.app/";
-  switch (fullUrl) {
-    case BASE_PATH:
-      return "Inicio";
-    case BASE_PATH + "nosotros/nuestro-equipo":
-      return "Nuestro equipo";
-    case BASE_PATH + "nosotros/nuestra-historia":
-      return "Nuestra historia";
-    case BASE_PATH + "nosotros/nuestros-valores":
-      return "Nuestros valores";
-    case BASE_PATH + "nosotros/nuestras-oficinas":
-      return "Nuestras oficinas";
-    case BASE_PATH + "nosotros/partners":
-      return "Partners";
-    case BASE_PATH +"publicaciones/historia-de-clientes":
-      return "Historia de clientes";
-    case BASE_PATH + "publicaciones/eventos":
-      return "Eventos";
-    case BASE_PATH + "empleo/programa-de-practicas":
-      return "Programa de practicas";
-    case BASE_PATH + "empleo/vacantes":
-      return "Vacantes";
-    case BASE_PATH + "empleo/trabaja-con-nosotros":
-      return "Trabaja con nosotros";
-    case BASE_PATH + "empleo/subir-curriculum":
-      return "Subir curriculum";
-    case BASE_PATH + "servicios/software-testing":
-      return "Software testing";
-    case BASE_PATH + "servicios/reclutamiento-it":
-      return "Reclutamiento IT";
-    case BASE_PATH + "servicios/gestion-de-proyectos":
-      return "Gestion de proyectos";
-    case BASE_PATH + "servicios/ux-ui":
-      return "UX / UI";
-    case BASE_PATH + "servicios/desarrollo-software":
-      return "Desarrollo de software";
-  }
 }
