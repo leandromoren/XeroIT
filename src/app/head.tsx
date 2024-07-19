1c59803821ef42cc443b2d4173743d2640cd3aaa
Leandro Moren <leandro.moren@outlook.com>
2024-07-19 18:12:57 -0300

    Se agrego 'Qavala - ' al title

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -58,7 +58,7 @@ export default function Head() {
 
   return (
     <header>
-      <title>{obtenerTituloEnProduccion()}</title>
+      <title>{"Qavala - " + obtenerTituloEnProduccion()}</title>
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1.0"
