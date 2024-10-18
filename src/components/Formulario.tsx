2b82c3938e8b2e85890b2ce18abac83605be8d17
Leandro Moren <leandro.moren@outlook.com>
2024-10-18 03:03:11 -0300

    Nuevo diseño del formulario principal

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,303 +1,35 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import {
-  Button,
-  Form,
-  Input,
-  Select,
-  InputNumber,
-  Alert,
-  Checkbox,
-} from "antd";
-import countries from "../fixtures/countries.json";
-import servicesData from "../fixtures/servicesData.json";
+import React from "react";
 import styles from "../styles/Formulario.module.css";
-import { TTexts } from "@/utils/textConstants";
-import axios from "axios";
-import { Mentions } from 'antd';
 
 export default function Formulario() {
-  const [country, setCountries] = useState(countries)
-
-  const [data, setData] = useState(servicesData)
-
-  const [form] = Form.useForm();
-
-  const [error, setError] = useState(false);
-
-  const [success, setSuccess] = useState(false);
-
-  const [selectedServices, setSelectedServices] = useState<string[]>([]);
-
-  const handleCheckboxChange = (checkedValues: string[]) => {
-    setSelectedServices(checkedValues);
-  };
-
-  const handleClose = () => {
-    setSuccess(false);
-  };
-
-  const actualizarPagina = () => {
-    return window.location.reload();
-  }
-
-  const onFinish = async (values: any) => {
-    setError(false);
-    try {
-      const response = await axios.post(
-        "https://formspree.io/xwpeegww",
-        {
-          ...values,
-          servicio: selectedServices,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      if (response.status === 200) {
-        setSuccess(true);
-        form.resetFields();
-        actualizarPagina();
-      } else {
-        setError(true);
-      }
-    } catch (error) {
-      console.error("Error submitting the form:", error);
-      setError(true);
-    }
-  };
-
-  useEffect(() => {
-    setData(servicesData);
-    setCountries(countries);
-  }, []);
-
-  const layout = {
-    labelCol: {
-      span: 6,
-    },
-    wrapperCol: {
-      span: 100,
-    },
-  };
-
   return (
-    <div className={styles.container}>
-      <div className={styles.formContainer}>
-        <Form
-          onFinish={onFinish}
-          method="POST"
-          form={form}
-          className={styles.form}
-          {...layout}
-          name="wrap"
-          labelCol={{
-            flex: "150px",
-          }}
-          labelAlign="right"
-          wrapperCol={{
-            flex: 1,
-          }}
-          colon={false}
-          autoComplete="off"
-        >
-          <div className={styles.formHeader}>
-            <h2 className={styles.formTitle}>Formulario de Contacto</h2>
-            <p className={styles.formDescription}>{TTexts.pTextForm}</p>
-          </div>
-          <Form.Item
-            label="Pais de residencia:"
-            name="pais"
-            rules={[
-              {
-                required: true,
-                message: "Selecciona tu país de residencia",
-              },
-            ]}
-          >
-            <Select value={"pais"} placeholder="Selecciona tu país">
-              {country.map((c) => (
-                <Select.Option key={c.code} value={c.name}>
-                  {c.name}
-                </Select.Option>
-              ))}
-            </Select>
-          </Form.Item>
-          <Form.Item
-            label="Servicios:"
-            name="servicio"
-            id="itemCheckbox"
-            rules={[
-              {
-                required: true,
-                message: "Selecciona al menos un servicio",
-              },
-            ]}
-          >
-            <Checkbox.Group
-              value={selectedServices}
-              onChange={handleCheckboxChange}
-              className={styles.checkboxGroup}
-            >
-              {data.map((service) => (
-                <Checkbox
-                  name="servicio"
-                  value={service.name}
-                  key={service.id}
-                  className={styles.serviceCheckbox}
-                >
-                  {service.name}
-                </Checkbox>
-              ))}
-            </Checkbox.Group>
-          </Form.Item>
-          <Form.Item
-            label="Nombre completo:"
-            name="nombre"
-            rules={[
-              {
-                required: true,
-                message: "Ingresa un nombre.",
-              },
-              {
-                pattern: /^[A-Za-z\s]+$/,
-                message: "El nombre no debe contener caracteres especiales"
-              }
-            ]}
-          >
-            <Input
-              placeholder="John Doe"
-              type="text"
-              name="nombre"
-              style={{
-                maxWidth: 250,
-              }}
-            />
-          </Form.Item>
-          <Form.Item
-            label="Empresa"
-            name="empresa"
-            style={{
-              maxWidth: 600,
-            }}
-            rules={[
-              {
-                required: true,
-                message: "Ingresa el nombre de tu empresa.",
-              },
-            ]}
-          >
-            <Input type="text" name="empresa" placeholder="Qavala" />
-          </Form.Item>
-          <Form.Item
-            label="Email de contacto:"
-            name="email"
-            rules={[
-              {
-                required: true,
-                message: "Ingresa un email válido",
-              },
-            ]}
-          >
-            <Input placeholder="info@example.com" name="email" type="text" />
-          </Form.Item>
-          <Form.Item
-            label="Tel. / Cel."
-            name="celular"
-            rules={[
-              {
-                required: true,
-                message: "Ingresa un número telefónico",
-              },
-            ]}
-          >
-            <InputNumber
-              placeholder="+541234567890"
-              name="celular"
-              type="number"
-              style={{
-                minWidth: "70%",
-              }}
-            />
-          </Form.Item>
-          <Form.Item
-            label="Describe tu idea:"
-            name="descripcion"
-            rules={[
-              {
-                required: true,
-                message: "Ingresa una descripción...",
-              },
-            ]}
-          >
-            <Mentions
-              placeholder="Describe tu idea..."
-              allowClear
-              rows={3}
-            />
-          </Form.Item>
-          <Form.Item
-            name="condiciones"
-            valuePropName="checked"
-            className={styles.containerTerms}
-            rules={[
-              {
-                validator: (_, value) =>
-                  value
-                    ? Promise.resolve()
-                    : Promise.reject(
-                      new Error("Debes aceptar los términos y condiciones.")
-                    ),
-              },
-            ]}
-          >
-            <Checkbox>
-              He leído y acepto los{" "}
-              <a href="/terminos-y-condiciones" className={styles.terms}>
-                términos
-              </a>{" "}
-              y{" "}
-              <a href="/terminos-y-condiciones" className={styles.terms}>
-                condiciones
-              </a>
-            </Checkbox>
-          </Form.Item>
-          <div className={styles.recaptchaContainer}></div>
-          <br />
-          {error ? (
-            <>
-              <Alert
-                message="Hubo un error al enviar los datos del formulario."
-                type="error"
-                closable
-                afterClose={handleClose}
-              />
-            </>
-          ) : null}
-          {success ? (
-            <>
-              <Alert
-                message="Formulario enviado exitosamente."
-                type="success"
-                closable
-                afterClose={handleClose}
-              />
-            </>
-          ) : null}
-          <br />
-          <Form.Item style={{ display: "flex", justifyContent: "center" }}>
-            <Button
-              className={styles.formSubmitButton}
-              htmlType="submit"
-              type="primary"
-            >
-              Solicitar servicio
-            </Button>
-          </Form.Item>
-        </Form>
+    <form>
+      <div className={styles.formHeader}>
+        <h1>Contáctanos</h1>
+        <h4>A nosotros nos encanta escucharte!</h4>
       </div>
-    </div>
-  );
+      <div className={styles.formContainer}>
+        <div className={styles.inputContainer}>
+          <div className={`${styles.inputWrapper} ${styles.wide}`}>
+            <input className={styles.input} type="text" required />
+            <label className={styles.label}>Nombre</label>
+          </div>
+          <div className={`${styles.inputWrapper} ${styles.half}`}>
+            <input className={styles.input} type="email" required />
+            <label className={styles.label}>Correo electrónico</label>
+          </div>
+          <div className={`${styles.inputWrapper} ${styles.half}`}>
+            <input className={styles.input} type="number" required />
+            <label className={styles.label}>Cel. Tel.</label>
+          </div>
+          <div className={`${styles.inputWrapper} ${styles.wide}`}>
+            <textarea className={styles.textarea} required></textarea>
+            <label className={styles.label}>Mensaje</label>
+          </div>
+          <button className={styles.submitBtn}>Enviar mensaje</button>
+        </div>
+      </div>
+    </form>
+  )
 }
