26ec29111d9dff463070e52922381cec073f7087
Leandro Moren <leandro.moren@outlook.com>
2024-10-20 17:08:08 -0300

    Agrego validacion del formulario

--- src/app/politica-de-privacidad/page.tsx
+++ src/app/politica-de-privacidad/page.tsx
@@ -1,18 +1,18 @@
-"use client"
-import Footer from "@/components/Footer"
-import Navbar from "@/components/NavBar"
-import PoliticasDePrivacidadVista from "@/views/PoliticasDePrivacidadVista"
-import { ChakraProvider } from "@chakra-ui/react"
-import React from "react"
+"use client";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/NavBar";
+import PoliticasDePrivacidadVista from "@/views/PoliticasDePrivacidadVista";
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
 
 export default function PoliticaDePrivacidadPage() {
-    return (
-        <>
-           <ChakraProvider>
-                <Navbar />
-                <PoliticasDePrivacidadVista />
-                <Footer />
-           </ChakraProvider>
-        </>
-    )
+  return (
+    <>
+      <ChakraProvider>
+        <Navbar />
+        <PoliticasDePrivacidadVista />
+        <Footer />
+      </ChakraProvider>
+    </>
+  );
 }
--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,8 +1,52 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/Formulario.module.css";
 
 export default function Formulario() {
+  const [errors, setErrors] = useState({
+    nombre: "",
+    email: "",
+    telefono: "",
+    mensaje: ""
+  });
+
+  const validateForm = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault(); // Prevenir que el formulario se envíe automáticamente
+    const nombre: string = (window.document.getElementById('nombre') as HTMLInputElement).value;
+    const email: string = (window.document.getElementById('email') as HTMLInputElement).value;
+    const telefono: string = (window.document.getElementById('telefono') as HTMLInputElement).value;
+    const mensaje: string = (window.document.getElementById('mensaje') as HTMLInputElement).value;
+
+    let hasError = false;
+    const newErrors = { nombre: "", email: "", telefono: "", mensaje: "" };
+
+    if (nombre.trim() === '') {
+      newErrors.nombre = 'El nombre es requerido.';
+      hasError = true;
+    }
+    if (telefono.trim() === '') {
+      newErrors.telefono = 'El teléfono es requerido.';
+      hasError = true;
+    }
+    if (mensaje.trim() === '') {
+      newErrors.mensaje = 'El mensaje es requerido.';
+      hasError = true;
+    }
+    
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      newErrors.email = 'El correo electrónico no es válido.';
+      hasError = true;
+    }
+
+    setErrors(newErrors);
+
+    if (!hasError) {
+      // Enviar formulario
+      console.log("Formulario válido");
+    }
+  };
+
   return (
     <form method="POST">
       <div className={styles.formHeader}>
@@ -12,24 +56,28 @@ export default function Formulario() {
       <div className={styles.formContainer}>
         <div className={styles.inputContainer}>
           <div className={`${styles.inputWrapper} ${styles.wide}`}>
-            <input className={styles.input} type="text" required />
+            <input className={styles.input} id="nombre" type="text" />
             <label className={styles.label}>Nombre</label>
+            {errors.nombre && <p className={styles.errorMessage}>{errors.nombre}</p>}
           </div>
           <div className={`${styles.inputWrapper} ${styles.half}`}>
-            <input className={styles.input} type="email" required />
+            <input className={styles.input} id="email" type="email" />
             <label className={styles.label}>Correo electrónico</label>
+            {errors.email && <p className={styles.errorMessage}>{errors.email}</p>}
           </div>
           <div className={`${styles.inputWrapper} ${styles.half}`}>
-            <input className={styles.input} type="number" required />
+            <input className={styles.input} id="telefono" type="number" />
             <label className={styles.label}>Tel.</label>
+            {errors.telefono && <p className={styles.errorMessage}>{errors.telefono}</p>}
           </div>
           <div className={`${styles.inputWrapper} ${styles.wide}`}>
-            <textarea className={styles.textarea} required></textarea>
+            <textarea className={styles.textarea} id="mensaje"></textarea>
             <label className={styles.label}>Mensaje</label>
+            {errors.mensaje && <p className={styles.errorMessage}>{errors.mensaje}</p>}
           </div>
-          <button className={styles.submitBtn}>Enviar mensaje</button>
+          <button onClick={validateForm} className={styles.submitBtn}>Enviar mensaje</button>
         </div>
       </div>
     </form>
-  )
+  );
 }
